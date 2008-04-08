eabb9520261e4a271e24cd9fe14ee13dc31fd3b0
Robert Spanton <rspanton@zepler.net>
2008-04-08 14:11:24 +0000

    pyenv: Process xbout's output properly.
    git-svn-id: file:///home/rob/sr/dev/git-migration/svn/slug/pyenv/trunk@1340 98f67ba8-b320-0410-bda0-b753e294843c

--- radio.py
+++ radio.py
@@ -1,5 +1,10 @@
-import subprocess, select, os, re
+import subprocess, select, os, re, time
 import power
+
+lpath = "/usr/local/lib:."
+if os.getenv("LD_LIBRARY_PATH") != None:
+    lpath = os.getenv("LD_LIBRARY_PATH") + ":" + lpath
+os.putenv("LD_LIBRARY_PATH", lpath)
 
 def decode_str(s):
     r = re.compile("([^= ]+)=([^= ]+)")
@@ -16,26 +21,33 @@ def decode_str(s):
     return settings
 
 def wait_start():
-        xbout = subprocess.Popen(["./xbout"],
-                                 stdout = subprocess.PIPE)
-        fifo = xbout.stdout.fileno()
-        lstate = 0
-        text = ""
-        start_found = True
+    # Wait for xbd to start:
+    while not os.path.exists("/tmp/xbee"):
+        time.sleep(0.1)
 
-        # Wait for the START signal
-        while True:
-            if select.select([fifo], [], [], 0.5) == ([],[],[]):
-                # Flash the LED to indicate we're waiting
-                lstate = lstate ^ 8
-                power.setleds(lstate)
+    xbout = subprocess.Popen(["./xbout"],
+                             stdout = subprocess.PIPE)
+    fifo = xbout.stdout.fileno()
+    lstate = 0
+    text = ""
+    start_found = False
+
+    # Wait for the START signal
+    while True:
+        if select.select([fifo], [], [], 0.5) == ([],[],[]):
+            # Flash the LED to indicate we're waiting
+            lstate = lstate ^ 8
+            power.setleds(lstate)
+        else:
+            # Read the output
+            text += os.read(fifo, 1)
+
+            if start_found and text[-1] == "\n":
+                text = text[6:-1]
+                print "Received START:", text
+                return decode_str(text)
             else:
-                # Read the output
-                text += os.read(fifo, 1)
-
-                if start_found and text[-1] == "\n":
-                    text = text[7:-1]
-                    return decode_str(text)
-                else:
-                    if text[-6:] == "START:":
-                        start_found = True
+                if text[0:6] != "START:"[0:len(text)]:
+                    text = ""
+                elif text[0:6] == "START:":
+                    start_found = True
