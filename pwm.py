c596ff06259bd577bc4a52983eca214a6708c9b3
Chris Cross <cjcross@zepler.net>
2009-04-12 17:05:42 +0000

    PWM.PY: This commit adds setlcd(...) to the sr python environment. This function writes the given text to the given screen buffer. It can be called as setlcd("text") where the default screen is screen 0. or it can be called setlcd(screen_no, "text"). Text, lists, or numbers can be supplied and will be displayed as strings. This routine uses readback and checksums. It has been tested with motor board connected as well, but not with motors plugged in.
    git-svn-id: file:///home/rob/sr/dev/git-migration/svn/slug/pyenv/trunk@2711 98f67ba8-b320-0410-bda0-b753e294843c

--- pwm.py
+++ pwm.py
@@ -5,8 +5,12 @@ ADDRESS = 0x2E
 
 PWM_CMD_SET = 1
 PWM_CMD_GET_LAST = 2
-LCD_SCREEN_STRING = 8
+LCD_CMD_SET_POS = 6
+LCD_CMD_GET_POS = 7
+LCD_CMD_WRITE = 8
+LCD_CMD_CSUM = 9
 status = [-1, -1, -1, -1, -1, -1]
+
 
 def readpos( s ):                                                                              
         #Read the position of the specified servo                                            
@@ -43,22 +47,48 @@ def setpos( s, pos ):
 			status[s] = pos
 			break	
 
-def setlcd(screen, text):
-	"""Write upto 32 characters of text to the lcd"""
+
+def setlcd(*args):
+	"""Write up to 32 characters of text to the lcd"""
+
+	if len(args) == 1:
+		screen = 0
+		text = args[0]
+	elif len(args) == 2:
+		screen = args[0]
+		text = args[1]
+	else:
+		raise TypeError, "setlcd takes 1 or 2 arguments only. e.g. setlcd(0, \"text\") or setlcd(\"text\")"	
 
 	if screen not in [0, 1, 2, 3]:
-		screen = 0;
+		raise TypeError, "Screen out of range. screen must be: 0, 1, 2, 3"
 
-	text = list(text[0:33])	#curtail
-	vals = [ord(' ')]*33
+
+	while True:			#set the screen to write to
+		setword(ADDRESS, LCD_CMD_SET_POS, screen)
+		if (0x3 & getbyte(ADDRESS, LCD_CMD_GET_POS) == screen):
+			break
+
+	vals = [ord(' ')]*32
 	i = 0
-	while(i < 32):
+	text = str(text)				#permit setlcd(54.4)	etc.
+	while( i < len(text) and (i < 32) ):		#convert to ascii
 		vals[i] = ord(text[i])
 		i += 1
 
-	powerwrite(ADDRESS, LCD_SCREEN_STRING, list(vals));
-	
+	tx_csum = 0;			#generate checksum
+	for i in vals:
+		tx_csum += i;
+	tx_csum &= 0xff
+				
+	while True:			#write screen contents
+		powerwrite(ADDRESS, LCD_CMD_WRITE, list(vals));
+		rx_csum = 0xff & getbyte(ADDRESS, LCD_CMD_CSUM)
+		if(rx_csum == tx_csum):
+			break
+
 class Pwm:
+
 	def __getitem__(self, n):
 		"Return the named servo's position"
 		return readpos(n)
