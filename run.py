2e34b648168e7ba3a04825a13b5b02889897dd58
Robert Spanton <rspanton@zepler.net>
2009-08-26 19:16:51 +0000

    pyenv: Add two more options allowing pyenv to be run without various bits of hardware.
    Adds '--no-xbee' option, and '--start' option.  The former does what
    it says on the tin.  The latter starts the robot code immediately,
    rather than waiting for a button press etc.
    
    git-svn-id: file:///home/rob/sr/dev/git-migration/svn/boards/slug/pyenv/trunk@3281 98f67ba8-b320-0410-bda0-b753e294843c

--- run.py
+++ run.py
@@ -9,12 +9,29 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
                     stream = sys.stdout)
 
+# Debug mode
 debug = False
+# Whether to wait for a start event
+wait_start_event = True
+# Whether to use the xbee
+use_xbee = True
 
 if len(sys.argv) > 1:
     for a in sys.argv[1:]:
         if a == "-d":
             debug = True
+        elif a == "--start":
+            wait_start_event = False
+        elif a == "--no-xbee":
+            use_xbee = False
+        elif a == "--help":
+            print """Usage: %s ARGS
+Where ARGS can be:
+ -d		Send stdout and stderr to terminal, not log.txt.
+ --start	Start the robot code immediately.
+		Don't wait for a button or radio event.
+ --no-xbee	Don't use the xbee.  Don't start xbd.""" % os.path.basename(sys.argv[0])
+            sys.exit(0)
 
 if not debug:
     sys.stderr = sys.stdout = open("log.txt", "at")
@@ -35,21 +52,28 @@ try:
     import trampoline
     print "Trampoline imported"
 
-    print "Starting xbd, the radio server"
-    # Reset the XBee
-    power.xbee_reset(True)
-    power.xbee_reset(False)
+    if use_xbee:
+        print "Starting xbd, the radio server"
+        # Reset the XBee
+        power.xbee_reset(True)
+        power.xbee_reset(False)
 
-    # Start xbd
-    xblog = open("xbd-log.txt","at")
-    subprocess.Popen(["./xbd",
-                      "-s", "/dev/ttyS0",
-                      "-b","57600",
-                      "--init-baud", "9600"],
-                     stdout = xblog, stderr = xblog )
+        # Start xbd
+        xblog = open("xbd-log.txt","at")
+        subprocess.Popen(["./xbd",
+                          "-s", "/dev/ttyS0",
+                          "-b","57600",
+                          "--init-baud", "9600"],
+                         stdout = xblog, stderr = xblog )
 
     t = trampoline.Trampoline()
-    add_coroutine( radio.xbee_monitor )
+    if wait_start_event:
+        add_coroutine( radio.xbee_monitor )
+    else:
+        print "Starting code"
+        import addhack, robot
+        addhack.add_queued()
+        addhack.add_coroutine( robot.main, games.GOLF, colours.RED )
 
     print "Starting trampoline"
     t.schedule()
