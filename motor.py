bf73f8074d06d5ab0e64189d1014f892d08df00d
Robert Spanton <rspanton@zepler.net>
2009-08-26 02:49:52 +0000

    pyenv: Implement the new motor API.
    Support for sensors and control.  API overview can be found on the wiki.
    
    git-svn-id: file:///home/rob/sr/dev/git-migration/svn/boards/slug/pyenv/trunk@3269 98f67ba8-b320-0410-bda0-b753e294843c

--- motor.py
+++ motor.py
@@ -1,120 +1,282 @@
 import c2py
 from repeat import *
+# Whether to display i2c debug info
+DEBUG_I2C = False
 
 ADDRESS = 0x12
 
-# Command
-MOTOR_IDENT = 0x00
-MOTOR_CMD_CONF = 0x01
-MOTOR_GET0 = 0x02
-MOTOR_GET1 = 0x03
+# Identify command
+IDENTIFY = 0x00
 
-# Directions
-OFF = 0
-FORWARD = 1
-BACKWARD = 2
-BRAKE = 3
+INTEGER = 1
+BOOL = 2
+ARRAY = 3
 
-def reg_read( reg, l ):
-    """Read a motor controller register.
-    reg is the register number.
-    l is the length of the register in byte.
-    Returns array of received bytes."""
-    while True:
-        try:
-            r = c2py.motor_reg_read( ADDRESS, reg, l )
-            break
-        except c2py.I2CError:
-            pass
-    return r
+TYPE_U8 = { "type": INTEGER, "len": 1, "signed": False }
+TYPE_U16 = { "type": INTEGER, "len": 2, "signed": False }
+TYPE_U32 = { "type": INTEGER, "len": 4, "signed": False }
+TYPE_I8 = { "type": INTEGER, "len": 1, "signed": True }
+TYPE_I16 = { "type": INTEGER, "len": 2, "signed": True }
+TYPE_I32 = { "type": INTEGER, "len": 4, "signed": True }
+TYPE_BOOL = { "type": BOOL }
+# Array of things
+TYPE_ARR = { "type": ARRAY }
 
-def reg_write( reg, data ):
-    while True:
-        try:
-            c2py.motor_reg_write( ADDRESS, reg, data )
-            break
-        except c2py.I2CError:
-            pass
+# Types of sensors:
+SENSOR_NULL = 0
+SENSOR_ADS5030 = 1
+# Types of controllers:
+CONTROLLER_UNITY = 0
+CONTROLLER_PID = 1
 
-def setpower_channel(channel, speed):
-    speed = float(speed)
+class Command:
+    def __init__(self, reg, rtype, rlen = None, ro = False):
+        self.reg = reg
+        self.rtype = rtype
+        self.len = rlen
+        self.ro = ro
 
-    if speed > 100:
-        speed = 100
-    elif speed < -100:
-        speed = -100
+    def read(self):
+        if self.rtype["type"] == INTEGER:
+            d = self._reg_read( self.rtype["len"] )
+            assert len(d) == self.rtype["len"]
 
-    if channel not in [0,1]:
-        raise ValueError, "Incorrect channel set"
-    
-    if speed > 0:
-        dir = FORWARD
-    else:
-        dir = BACKWARD
+            val = 0
+            # Switch to most significant byte first
+            d.reverse()
+            for x in d:
+                val <<= 8
+                val |= x
 
-    speed = abs(int(speed * 3.28))
-    setpower_raw( channel, speed, dir )
+            if self.rtype["signed"]:
+                "Sign extend"
+                if val & (1 << (len(d)*8-1)):
+                    "Most significant bit set -- needs sign extending"
+                    # In python, -1 is an infinite string of '1' bits
+                    val |= -1 << (len(d)*8)
 
-def setpower_raw( channel, speed, dir ):
-    v = speed | (dir << 9) | (channel<<11)
-    while True:
-        try:
-            c2py.writeworddata( ADDRESS, MOTOR_CMD_CONF, v, 1 )
-            break
-        except c2py.I2CError:
-            continue
+            return val
 
-        _dir, _speed = __getpower_raw( channel )
+        elif self.rtype["type"] == BOOL:
+            d = self._reg_read( 1 )
+            if d[0]:
+                return True
+            return False
 
-        if _dir == dir and _speed == speed:
-            break
+    def write(self, v):
+        if self.rtype["type"] == INTEGER:
+            d = []
+            for p in range(0,self.rtype["len"]):
+                d.append( v & 0xff )
+                v >>= 8
 
-def __getpower_raw( channel ):
-    cmd = (MOTOR_GET0, MOTOR_GET1)[channel]
-    n = getword( ADDRESS,  cmd )
+            self._reg_write(d)
 
-    speed = n & 0x1FF
-    dir = n>>9
-    return (dir,speed)
+        elif self.rtype["type"] == BOOL:
+            if v:
+                self._reg_write([1])
+            else:
+                self._reg_write([0])
 
-def __getpower(channel):
-	if channel not in [0, 1]:
-		raise ValueError, "Incorrect channel read"
+    def _reg_read( self, l ):
+        """Read a motor controller register.
+        reg is the register number.
+        l is the length of the register in byte.
+        Returns array of received bytes."""
+        while True:
+            try:
+                r = c2py.motor_reg_read( ADDRESS, self.reg, l )
+                break
+            except c2py.I2CError:
+                pass
+        self._debug("Read", r)
+        return r
 
-        dir, speed = __getpower_raw(channel)
-	
-	if(dir == OFF or dir == BRAKE):
-		return 0
-	elif dir == FORWARD:
-		return speed/3.28
-	elif dir == BACKWARD:
-		return -1*speed/3.28
-	
-def setpower(*args):
-    if len(args) == 1:
-        setpower_channel( 0, args[0] )
-        setpower_channel( 1, args[0] )
-    elif len(args) == 2:
-        setpower_channel( 0, args[0] )
-        setpower_channel( 1, args[1] )
-    else:
-        raise TypeError, "setspeed takes one or two numeric arguments"
+    def _reg_write( self, data ):
+        self._debug("Write", data)
+        while True:
+            try:
+                c2py.motor_reg_write( ADDRESS, self.reg, data )
 
-def readpower(*args):
-	if len(args) == 1:
-		return	__getpower(args[0])
-	else:
-		raise TypeError, "readpower takes only one numeric argument"
+                if self._reg_read( len(data) ) == data:
+                    break
+            except c2py.I2CError:
+                pass
 
-class Motors:
+    def _debug(self, s, data):
+        if not DEBUG_I2C:
+            return
+        print "%s(%i):" % (s, self.reg),
+        for b in data:
+            print "%x" % b, 
+        print
+
+class CommandSet:
+    def __init__(self, channel):
+        if channel == 0:
+            controller = 16
+            sensor = 32
+            control = 48
+        else:
+            controller = 64
+            sensor = 80
+            control = 96
+
+        # Identity
+        self.IDENTITY = Command( 0, TYPE_U32 )
+
+        # PID controller
+        self.PID_KP = Command( controller+0, TYPE_I16 )
+        self.PID_KI = Command( controller+1, TYPE_I16 )
+        self.PID_KD = Command( controller+2, TYPE_I16 )
+
+        # ADS5030 sensor
+        self.ADS5030_CLK = Command( sensor+0, TYPE_U8 )
+        self.ADS5030_DIO = Command( sensor+1, TYPE_U8 )
+        self.ADS5030_SHR = Command( sensor+2, TYPE_U8 )
+
+        ## Control loop:
+        self.CONTROL_ENABLED = Command( control+0, TYPE_BOOL )
+        self.CONTROL_TARGET = Command( control+1, TYPE_I32 )
+        # Speed
+        self.CONTROL_SPEED_ENABLED = Command( control+2, TYPE_BOOL )
+        self.CONTROL_SPEED_INC = Command( control+3, TYPE_I16 )
+        self.CONTROL_SPEED_PERIOD = Command( control+4, TYPE_I16 )
+        # Controller and sensor selection
+        self.CONTROL_CONTROLLER = Command( control+5, TYPE_U8 )
+        self.CONTROL_SENSOR = Command( control+6, TYPE_U8 )
+
+CMD = [ CommandSet(0), CommandSet(1) ]
+
+class ADS5030Sensor:
+    def __init__(self, channel):
+        self.channel = channel
+        self.cmds = CMD[channel]
+
+    def set_pins(self, clk, dio):
+        if clk not in [0,1,2,3] or dio not in [0,1,2,3]:
+            raise "clk and dio must be between 0 and 3 inclusive"
+
+        self.cmds.ADS5030_CLK.write( 1 << clk )
+        self.cmds.ADS5030_DIO.write( 1 << dio )
+
+    def set_shr(self, shr):
+        self.cmds.ADS5030_SHR.write(shr)
+
+class PIDController:
+    def __init__(self, channel):
+        self.channel = channel
+        self.cmds = CMD[channel]
+
+    def set_coeff( self, kp, ki, kd ):
+        self.cmds.PID_KP.write( kp )
+        self.cmds.PID_KI.write( kp )
+        self.cmds.PID_KD.write( kp )
+
+class Motor:
+    def __init__(self, channel):
+        self.channel = channel
+        self.cmds = CMD[channel]
+
+        self._cur_sensor_attr = None
+        self._cur_controller_attr = None
+        self.__dict__["sensor"] = None
+        self.__dict__["controller"] = None
+
+    def enable(self):
+        self.cmds.CONTROL_ENABLED.write(True)
+
+    def disable(self):
+        self.cmds.CONTROL_ENABLED.write(False)
+
+    def __setattr__(self, n, v):
+        if n == "sensor":
+            self._switch_sensor(v)
+        elif n == "controller":
+            self._switch_controller(v)
+        elif n == "target":
+            self._set_target(v)
+        else:
+            self.__dict__[n] = v
+
+    def _set_target(self, t):
+        # Called by __setattr__ -- careful.
+
+        if self._cur_controller_attr == "UNITY":
+            # Max is 100, min is -100
+            if t > 100:
+                t = 100
+            elif t < -100:
+                t = -100
+        
+        t = int(t)
+
+        self.cmds.CONTROL_TARGET.write(t)
+        self.__dict__["target"] = t
+
+    def _switch_sensor(self, v):
+        # Called by __setattr__ -- careful.
+        prev = self._cur_sensor_attr
+
+        if v == Motors.NULL:
+            self.__dict__["sensor"] = Motors.NULL
+            self.NULL = None
+            self._cur_sensor_attr = "NULL"
+            num = SENSOR_NULL
+
+        elif v == Motors.ADS5030:
+            self.__dict__["sensor"] = Motors.ADS5030
+            self.ads5030 = ADS5030Sensor(self.channel)
+            self._cur_sensor_attr = "ads5030"
+            num = SENSOR_ADS5030
+
+        else:
+            raise "Invalid sensor requested"
+
+        self.cmds.CONTROL_SENSOR.write(num)
+
+        if prev != None and self._cur_sensor_attr != prev :
+            "Get rid of the old sensor's properties"
+            delattr(self, prev)
+
+    def _switch_controller(self, v):
+        # Called by __setattr__ -- careful.
+        prev = self._cur_controller_attr
+
+        if v == Motors.UNITY:
+            self.__dict__["controller"] = Motors.UNITY
+            self.UNITY = None
+            self._cur_controller_attr = "UNITY"
+            num = CONTROLLER_UNITY
+
+        elif v == Motors.PID:
+            self.__dict__["controller"] = Motors.PID
+            self.PID = PIDController(self.channel)
+            self._cur_controller_attr = "PID"
+            num = CONTROLLER_PID
+
+        else:
+            raise "Invalid controller requested"
+
+        self.cmds.CONTROL_CONTROLLER.write(num)
+
+        if prev != None and self._cur_controller_attr != prev:
+            "Get rid of the old controller's properties"
+            delattr(self, prev)
+
+
+class Motors(list):
+    # Sensor types
+    NULL = "null sensor"
+    ADS5030 = "ads5030 sensor"
+
+    # Controller types:
+    UNITY = "unity controller"
+    PID = "pid controller"
+
     def __init__(self):
-        pass
-
-    def __getitem__(self, n):
-        "Return the current motor speed"
-        return readpower(n)
-
-    def __setitem__(self, n, v):
-        setpower_channel(n, v)
+        list.__init__(self)
+        self.append( Motor(0) )
+        self.append( Motor(1) )
 
 motor = Motors()
