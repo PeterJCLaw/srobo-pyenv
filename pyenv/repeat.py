bad0dafebf873b67aa923f73feb52cee3e0419bc
Chris Kirkham <chrisjameskirkham@gmail.com>
2010-09-09 17:46:45 +0100

    Removed mentions of c2py in repeat.py, replacing with 'NotImplemeted'

--- pyenv/repeat.py
+++ pyenv/repeat.py
@@ -1,65 +1,29 @@
-import c2py
 
 def getbyte(address, cmd):
-    while True:
-        try:
-            byte = c2py.readbytedata( address, cmd, 1 )
-            break
-        except c2py.I2CError:
-            pass
-    return byte
+
+    return NotImplemented
 
 def setword(address, cmd, val):
-    while True:
-        try:
-            c2py.writeworddata(address, cmd, val, 1)
-            break
-        except c2py.I2CError:
-            pass
+
+    return NotImplemented
 
 def setbyte(address, cmd, val):
-    while True:
-        try:
-            c2py.writebytedata(address, cmd, val, 1)
-            break
-        except c2py.I2CError:
-            pass
+
+    return NotImplemented
 
 def getword(address, cmd):
-    while True:
-        try:
-            val = c2py.readworddata(address, cmd, 1)
-            break
-        except c2py.I2CError:
-            pass
 
-    return val
+    return NotImplemented
 
 def getblock(address, cmd, bytes):
-    while True:
-        try:
-            val = c2py.readblockdata(address, cmd, bytes)
-            break
-        except c2py.I2CError:
-            pass
 
-    return val
+    return NotImplemented
 
 # following two functions to read and write to power board only
 def powerread(address, cmd):
-    while True:
-        try:
-            val = c2py.powerread(address, cmd)
-            break
-        except c2py.I2CError:
-            pass
 
-    return val
+    return NotImplemented
 
 def powerwrite(address, cmd, val):
-    while True:
-        try:
-            c2py.powerwrite(address, cmd, val)
-            break
-        except c2py.I2CError:
-            pass
+
+    return NotImplemented
