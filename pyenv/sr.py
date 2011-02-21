bc6d64d0dff839f5567b803e882e92fc7db68e32
Robert Spanton <rspanton@zepler.net>
2011-02-21 21:08:03 +0000

    Remove much of the old I2C-based interface
    Leaving the juicy bits behind that are likely to be useful in the
    future, such as the IOPoll and event stuff.

--- pyenv/sr.py
+++ pyenv/sr.py
@@ -1,14 +1,8 @@
-from motor import motor
-
 from vis import vision
 from colours import *
-from pwm import pwm, setpos, readpos, setlcd, lcd
 import logging
 
 from poll import And, Or, TimePoll
 from jointio import io
 from addhack import add_coroutine, coroutine
 from time_event import timeout
-
-from power import setleds, getleds, power, setled, getled, power_switch
-
