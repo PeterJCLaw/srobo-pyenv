fe25a791cba641acfa04da0dd4f71590abf1272e
Robert Spanton <rspanton@zepler.net>
2011-02-23 02:10:10 +0000

    Use the __builtin__ module for modifying builtins
    This is the documented way of doing it.  It makes much more sense.
    
    However, it looks like it will be better to hook into the module
    importing code in the future and scan through the module to find
    decorated coroutines that way.  This might have the potential to
    completely skip the hacky globals approach.

--- pyenv/addhack.py
+++ pyenv/addhack.py
@@ -1,25 +1,22 @@
-# A horrible hack for adding new coroutines
-# Getting different modules to talk about the same list
-# was like pulling my eyes through a sieve.
-# Unfortunately, I don't have enough time to learn about
-# how Python things namespaces should work.
-# Putting the list in it's own module works.
+# A hack for maintaining a global list of coroutines to be executed
+# This allows the @coroutine decorator to function
+import __builtin__
 
 try:
-    n = len(__builtins__.__addhack_new_coroutines)
+    n = len(__builtin__.__addhack_new_coroutines)
 except:
-    __builtins__["__addhack_new_coroutines"] = []
-    __builtins__["__addhack_decorated"] = []
-    __builtins__["__queue_decorated"] = True
+    __builtin__.__addhack_new_coroutines = []
+    __builtin__.__addhack_decorated = []
+    __builtin__.__queue_decorated = True
 
 def add_coroutine(f, *args, **keys):
     print "Adding coroutine"
-    __builtins__["__addhack_new_coroutines"].append( (f, args, keys) )
+    __builtin__.__addhack_new_coroutines.append( (f, args, keys) )
 
 def coroutine(f):
     "Decorator to add a function as a coroutine "
-    if __builtins__["__queue_decorated"]:
-        __builtins__["__addhack_decorated"].append(f)
+    if __builtin__.__queue_decorated:
+        __builtin__.__addhack_decorated.append(f)
     else:
         add_coroutine(f)
 
@@ -27,18 +24,18 @@ def coroutine(f):
     return f
 
 def clear_coroutines():
-    __builtins__["__addhack_new_coroutines"] = []
+    __builtin__.__addhack_new_coroutines = []
 
 def get_coroutines():
-    return __builtins__["__addhack_new_coroutines"]
+    return __builtin__.__addhack_new_coroutines
 
 def show_coroutines():
-    print "%i coroutines queued" % len(__builtins__["__addhack_new_coroutines"])
+    print "%i coroutines queued" % len(__builtin__.__addhack_new_coroutines)
 
 def add_queued():
-    __builtins__["__queue_decorated"] = False
-    for x in __builtins__["__addhack_decorated"]:
+    __builtin__.__queue_decorated = False
+    for x in __builtin__.__addhack_decorated:
         add_coroutine(x)
-    __builtins__["__addhack_decorated"] = []
+    __builtin__.__addhack_decorated = []
 
     
