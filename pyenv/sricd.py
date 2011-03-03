00fd2e39b4433214640b98b6ffacbd718e0d3598
Robert Spanton <rspanton@zepler.net>
2011-03-03 23:16:40 +0000

    Add a delay after starting sricd to allow it to enumerate
    sricd doesn't currently wait for enumeration to finish before
    responding to requests from its clients.  This means that if it's
    queried before its finished enumerating, the clients get told there's
    nothing on the bus.

--- pyenv/sricd.py
+++ pyenv/sricd.py
@@ -1,5 +1,6 @@
 # Utility functions for starting/stopping sricd
 from subprocess import Popen
+import time
 
 def start(logfile):
     out = open( logfile, "a" )
@@ -8,3 +9,7 @@ def start(logfile):
     p = Popen( "./bin/sricd -d -u /dev/ttyS1 -v",
                stdout = out, stderr = out, shell = True )
     p.wait()
+
+    # Wait two seconds for enumeration and stuff to complete
+    # (otherwise libsric clients will get no devices listed)
+    time.sleep(2)
