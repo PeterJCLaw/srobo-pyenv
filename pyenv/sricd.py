71bd48d1acf09289a2ea29d657c8fdc0e24c991c
Robert Spanton <rspanton@zepler.net>
2011-03-03 23:49:00 +0000

    Kill sricd when run.py quits
    This is extremely useful for debugging purposes.  It means I no longer
    have to remember to kill sricd when restarting run.py.

--- pyenv/sricd.py
+++ pyenv/sricd.py
@@ -1,14 +1,35 @@
 # Utility functions for starting/stopping sricd
 from subprocess import Popen
-import time
+import __builtin__, os, signal
+import time, sys
+PID_FILE = "/tmp/sricd.pid"
+
+def _kill():
+    "Kill sricd"
+    print "Killing sricd"
+    sys.stdout.flush()
+
+    if os.path.exists(PID_FILE):
+        f = open(PID_FILE)
+        pid = int( f.read() )
+        f.close()
+
+        os.kill( pid, signal.SIGTERM )
+    else:
+        print "sricd file doesn't exist"
 
 def start(logfile):
     out = open( logfile, "a" )
     print >>out, "-" * 80
 
-    p = Popen( "./bin/sricd -d -u /dev/ttyS1 -v",
+    p = Popen( "./bin/sricd -p /tmp/sricd.pid -d -u /dev/ttyS1 -v",
+               stdin = open("/dev/null", "r"),
                stdout = out, stderr = out, shell = True )
     p.wait()
+
+    if hasattr(__builtin__, "__sr_trampoline"):
+        "We're in the trampoline -- kill sricd on exit"
+        __sr_cleanup_funcs.append( _kill )
 
     # Wait two seconds for enumeration and stuff to complete
     # (otherwise libsric clients will get no devices listed)
