e6e84f06db89ee904cf40c83c9b3ad3f30826ee6
Jeremy Morse <jmorse+git@studentrobotics.org>
2011-03-06 18:02:40 +0000

    s/array/list/

--- pyenv/hw/power.py
+++ pyenv/hw/power.py
@@ -1,6 +1,5 @@
 from jointio import IOPoll
 from events import Event
-from array import array
 import pysric
 
 CMD_ENABLE_INPUT_NOTES = 5
@@ -12,7 +11,7 @@ CMD_GET_LEDS = 9
 class Power:
     def __init__(self, dev):
         self.dev = dev
-        self.led = Power.LedArray('I', [0, 0, 0], dev)
+        self.led = Power.LedList('I', [0, 0, 0], dev)
 
     def beep( self, freq = 1000, dur = 0.1 ):
         "Beep"
@@ -43,13 +42,13 @@ class Power:
 
         self.dev.txrx( tx )
 
-    class LedArray(array):
+    class LedList(list):
         def __new__(cls, typeclass, init_values, dev):
-            return array.__new__(cls, typeclass, init_values)
+            return list.__new__(cls, typeclass, init_values)
 
         def __init__(self, typeclass, init_values, dev=None):
             # We receive a deprecation warning if the following is enabled
-            # array.__init__(self, typeclass, init_values)
+            # list.__init__(self, typeclass, init_values)
             self.dev = dev
 
         def __setitem__(self, idx, val):
