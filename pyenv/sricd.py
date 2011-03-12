c15b62183b52757ea47870a48b08e36555d890ea
Robert Spanton <rspanton@zepler.net>
2011-03-12 19:41:33 +0000

    Revert "Remove the shutdown stuff from sricd.py"
    This reverts commit b0ecf7e87db7893a3fc0e39561236a06432e83a5.
    
    This is exceptionally useful when debugging pyenv stuff.  Removing
    this feature was a cop-out at fixing the real problem.

--- pyenv/sricd.py
+++ pyenv/sricd.py
@@ -4,6 +4,20 @@ import __builtin__, os, signal
 import time, sys
 PID_FILE = "/tmp/sricd.pid"
 
+def _kill():
+    "Kill sricd"
+    print "Killing sricd"
+    sys.stdout.flush()
+
+    if os.path.exists(PID_FILE):
+        f = open(PID_FILE)
+        pid = int( f.read() )
+        f.close()
+
+        os.kill( pid, signal.SIGTERM )
+    else:
+        print "sricd file doesn't exist"
+
 def start(logfile):
     out = open( logfile, "a" )
     print >>out, "-" * 80
@@ -12,6 +26,10 @@ def start(logfile):
                stdin = open("/dev/null", "r"),
                stdout = out, stderr = out, shell = True )
     p.wait()
+
+    if hasattr(__builtin__, "__sr_trampoline"):
+        "We're in the trampoline -- kill sricd on exit"
+        __sr_cleanup_funcs.append( _kill )
 
     # Wait two seconds for enumeration and stuff to complete
     # (otherwise libsric clients will get no devices listed)
