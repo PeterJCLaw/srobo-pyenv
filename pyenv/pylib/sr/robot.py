08acb212388f45bb9ac4ae15fd62b10a0ea34184
Robert Spanton <rspanton@zepler.net>
2011-10-10 01:37:25 +0100

    Parse command-line arguments and data received through startfifo
    Add the data received through these methods to the Robot object in the
    following attributes:
     * usbkey
     * startfifo
     * zone
     * mode

--- pyenv/pylib/sr/robot.py
+++ pyenv/pylib/sr/robot.py
@@ -1,7 +1,47 @@
+import json, sys, optparse, time, os
 
 class Robot(object):
     """Class for initialising and accessing robot hardware"""
 
     def __init__(self):
-        pass
+        self._parse_cmdline()
+        self._wait_start()
+
+    def _parse_cmdline(self):
+        "Parse the command line arguments"
+        parser = optparse.OptionParser()
+
+        parser.add_option( "--usbkey", type="string", dest="usbkey",
+                           help="The path of the (non-volatile) user USB key" )
+
+        parser.add_option( "--startfifo", type="string", dest="startfifo",
+                           help="The path of the fifo which start information will be received through" )
+        (options, args) = parser.parse_args()
+
+        self.usbkey = options.usbkey
+        self.startfifo = options.startfifo
+
+    def _wait_start(self):
+        "Wait for the start signal to happen"
+
+        # The fifo might not exist yet, so wait for it to exist
+        while not os.path.exists( self.startfifo ):
+            time.sleep(0.2)
+
+        f = open( self.startfifo, "r" )
+        d = f.read()
+        f.close()
+
+        j = json.loads(d)
+
+        if "zone" not in j or "mode" not in j:
+            raise Exception( "'zone' and 'mode' must be in startup info" )
+
+        self.mode = j["mode"]
+        self.zone = j["zone"]
+
+        if self.mode not in ["comp", "dev"]:
+            raise Exception( "mode of '%s' is not supported -- must be 'comp' or 'dev'" % self.mode )
+        if self.zone < 0 or self.zone > 3:
+            raise Exception( "zone must be in range 0-3 inclusive -- value of %i is invalid" % self.zone )
 
