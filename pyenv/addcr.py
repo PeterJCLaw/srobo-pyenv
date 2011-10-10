9ae0259d09f3779abb28458c15ee3ab6cb80235b
Robert Spanton <rspanton@zepler.net>
2011-10-10 03:36:15 +0100

    Make AddCRWriter pass {set,get}attr through to the wrapped file object
    This means that things like fileno() and flush() work on it properly.

--- pyenv/addcr.py
+++ pyenv/addcr.py
@@ -13,3 +13,15 @@ class AddCRWriter(object):
     def write(self, string):
         string = string.replace('\n', '\r\n')
         self._actual.write(string)
+
+    def __getattr__(self, name):
+        "Defer attribute look-ups to the wrapped file object"
+        return getattr( self._actual, name )
+
+    def __setattr__(self, name, val):
+        "Defer attribute setting to the wrapped file object"
+        if name == "_actual":
+            self.__dict__[name] = val
+            return
+
+        setattr( self._actual, name, val )
