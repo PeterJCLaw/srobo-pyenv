fd7553e55f0207250bf414a3631f7cabf45bf355
Robert Spanton <rspanton@zepler.net>
2011-10-12 05:29:24 +0100

    Extend wait_for to take keyword arguments
    For example:
    
    	bump_sensor = R.io[0].input[0]
    	arm_sensor = R.io[0].input[1]
    
    	r = wait_for( arm = arm_sensor.d == 1,
    	    	      bump = bump_sensor.d == 1 )
    
    When wait_for returns, it'll return a namedtuple that can be accessed
    like so:
    
    	if r.arm != None:
    		...
    	elif r.bump != None:
    	     	...
    
    Note that the order of the arguments is irrelevant, since keyword
    arguments go through a dict.

--- pyenv/pylib/sr/event.py
+++ pyenv/pylib/sr/event.py
@@ -1,7 +1,35 @@
 "Event handling stuff"
+import collections
 
-def wait_for( *polls ):
+def wait_for_named( **named ):
+    C = collections.namedtuple( "WaitResults", named.keys )
+
+    happened = False
+    while not happened:
+        res = {}
+        for name, poll in named.iteritems():
+            happened, val = poll.eval()
+
+            if happened:
+                res[name] = val
+                break
+            else:
+                res[name] = None
+
+    # Set the remaining values to None:
+    for name in named.keys():
+        if name not in res:
+            res[name] = None
+
+    return C(**res)
+
+def wait_for( *polls, **named ):
     "Wait for at least one of the passed polls to happen"
+
+    if len(named):
+        if len(polls):
+            raise Exception("wait_for supplied with both normal and keyword arguments")
+        return wait_for_named( **named )
 
     happened = False
     while not happened:
