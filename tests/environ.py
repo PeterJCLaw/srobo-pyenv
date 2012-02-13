115d8bd099bb3ab4916b70fbee9900322ac1ee4d
Rob Spanton <rspanton@zepler.net>
2012-02-13 02:47:56 +0000

    Add unit tests for stdout newline munging and that assert works
    Two tests of the environment:
    
    1) That the stdout newline windowsifier is present.
    2) That assert statements work (we are not in optimisation mode)

---
+++ tests/environ.py
@@ -0,0 +1,25 @@
+import sr
+import unittest
+import sys
+
+class EnvironTest(unittest.TestCase):
+    "Tests of the execution environment in general"
+
+    def test_stdout_munger(self):
+        "Check that stdout newlines will be munged"
+        # Note that this test has to behave differently 
+        # when stdout is a tty, as the munging isn't done on that
+
+        if not sys.stdout.isatty():
+            self.assertTrue( isinstance( sys.stdout,
+                                         sr.loggrock.AddCRFlusher ) )
+
+    def test_assert_works(self):
+        """Check that assert works
+
+        The execfile situation can put us in debug mode.
+        This makes assert statements not work."""
+        def failfunc():
+            assert False
+
+        self.assertRaises( AssertionError, failfunc )
--- tests/robot.py
+++ tests/robot.py
@@ -1,11 +1,13 @@
 import unittest
 from unittest import TestLoader, TestSuite
-import jio, power
+import jio, power, environ
 
 jointio_tests = TestLoader().loadTestsFromTestCase(jio.JointIOTest)
 power_tests = TestLoader().loadTestsFromTestCase(power.PowerTest)
+environ_tests = TestLoader().loadTestsFromTestCase(environ.EnvironTest)
 
 suite = TestSuite( [ jointio_tests,
-                     power_tests ] )
+                     power_tests,
+                     environ_tests ] )
 
 unittest.TextTestRunner(verbosity=2).run(suite)
