707dfca25040859ba37a8dcce6a1c098aae827e6
Rob Spanton <rspanton@zepler.net>
2012-02-13 03:41:49 +0000

    Only perform JointIO tests if a JointIO board is present
    There's a variable that can be changed to True to force them to
    happen as well.

--- tests/robot.py
+++ tests/robot.py
@@ -1,13 +1,23 @@
 import unittest
 from unittest import TestLoader, TestSuite
 import jio, power, environ
+import sr
+
+FORCE_ALL_DEVS = False
 
 jointio_tests = TestLoader().loadTestsFromTestCase(jio.JointIOTest)
 power_tests = TestLoader().loadTestsFromTestCase(power.PowerTest)
 environ_tests = TestLoader().loadTestsFromTestCase(environ.EnvironTest)
 
-suite = TestSuite( [ jointio_tests,
-                     power_tests,
-                     environ_tests ] )
+R = sr.Robot()
+
+suite = TestSuite( [ environ_tests,
+                     power_tests ] )
+
+if len( R.io ) or FORCE_ALL_DEVS:
+    suite.addTests( [ jointio_tests ] )
+
+# We don't actually want this robot...
+del R
 
 unittest.TextTestRunner(verbosity=2).run(suite)
