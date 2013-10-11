a0cee64aa193c2b7d54e0984e37f6f3b11120a50
Rob Spanton <rspanton@zepler.net>
2013-10-11 17:47:26 +0100

    Ruggeduino: Add the beginnings of some docstrings

--- pyenv/pylib/sr/ruggeduino.py
+++ pyenv/pylib/sr/ruggeduino.py
@@ -8,6 +8,8 @@ OUTPUT = "OUTPUT"
 INPUT_PULLUP = "INPUT_PULLUP"
 
 class Ruggeduino(object):
+    """Class for talking to a Ruggeduino flashed with the SR firmware"""
+
     def __init__(self, path):
         self.serial = serial.Serial(path, SERIAL_BAUD, timeout=0.1)
         if not self._is_srduino():
@@ -17,6 +19,10 @@ class Ruggeduino(object):
         self.serial.close()
 
     def command(self, data):
+        """Send a command to the Ruggeduino
+
+        Returns the response from the device."""
+
         for i in range(10):
             self.serial.write(bytes(data))
             res = self.serial.readline()
@@ -25,6 +31,8 @@ class Ruggeduino(object):
         raise Exception("Communications with Ruggeduino failed")
 
     def _is_srduino(self):
+        "Determine if the board is flashed with the SR firmware"
+
         response = self.command('v')
         if response.split(":")[0] == "SRduino":
             return True
@@ -32,21 +40,26 @@ class Ruggeduino(object):
             return False
 
     def _encode_pin(self, pin):
+        "Encode a pin number in ascii"
         return chr(ord('a') + pin)
 
     def pin_mode(self, pin, mode):
+        "Set the mode of a pin"
         MODES = {INPUT: 'i',
                  OUTPUT: 'o',
                  INPUT_PULLUP: 'p'}
         self.command(MODES[mode] + self._encode_pin(pin))
 
     def digital_read(self, pin):
+        "Read a digital input"
         response = self.command('r' + self._encode_pin(pin))
         return True if response[0] == 'h' else False
 
     def digital_write(self, pin, level):
+        "Write to an output"
         self.command(('h' if level else 'l') + self._encode_pin(pin))
 
     def analogue_read(self, pin):
+        "Read an analogue input"
         response = self.command('a' + self._encode_pin(pin))
         return (int(response)/1023.0)*5.0
