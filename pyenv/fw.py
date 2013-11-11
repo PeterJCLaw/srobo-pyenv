2d33dbac430a65459bf1aa6b47c2b38adf895960
Rob Spanton <rspanton@zepler.net>
2013-11-11 19:43:58 +0000

    Stop fw splash from showing when there's no update
    The firmware-update splash screen would always display for a short
    period of time upon boot, regardless of whether an update was
    occuring.  This patch uses the power board's SRIC vbuf to determine
    whether an update is necessary, and then starts the splash screen if
    appropriate.
    
    Change-Id: I676f7da4e938d89b1dc55ba889901b3a94a43f99

--- pyenv/fw.py
+++ pyenv/fw.py
@@ -2,7 +2,35 @@
 # Routines for invoking flashb and thus updating board firmware.
 import subprocess, os.path, re
 
+SRIC_VERSION_BUF_CMD = 0x84
+
+power_vbuf = [ 7, 100, 114, 105, 118, 101, 114, 115, 5, 37,
+               251, 84, 30, 144, 8, 102, 108, 97, 115, 104,
+               52, 51, 48, 5, 87, 171, 113, 171, 124, 7,
+               108, 105, 98, 115, 114, 105, 99, 5, 31, 227,
+               112, 155, 220, 1, 46, 5, 230, 243, 165, 7,
+               161]
+
+def sric_read_vbuf(dev):
+    "Read the versionbuf from dev"
+    d = []
+    off = 0
+
+    while True:
+        "Loop until we've received all the buffer"
+        r = dev.txrx( [SRIC_VERSION_BUF_CMD, off & 0xff, (off >> 8) & 0xff] )
+        d += r
+        if len(r) == 0:
+            break
+        off += len(r)
+
+    return d
+
 def update_with_gui( root, bin_dir, log_dir ):
+    if not check_power_update():
+        "No update required"
+        return False
+
     p = subprocess.Popen( [ os.path.join( bin_dir, "fwsplash" ) ] )
 
     res = update_power( root, bin_dir, log_dir )
@@ -10,6 +38,13 @@ def update_with_gui( root, bin_dir, log_dir ):
     p.kill()
     p.wait()
     return res
+
+def check_power_update():
+    "Determine if a power board update is necessary using its vbuf"
+    import sr.pysric as pysric
+    p = pysric.PySric()
+    vb = sric_read_vbuf( p.devices[ pysric.SRIC_CLASS_POWER ][0] )
+    return vb != power_vbuf
 
 def update_power( root, bin_dir, log_dir ):
     logpath = os.path.join( log_dir, "fw-log.txt" )
