8352d8752d4d265469127d91c3267781e07a777a
Peter Law <PeterJCLaw@gmail.com>
2013-11-16 00:14:00 +0000

    Remove obsolete JointIO, event and poll API parts
    These aren't being used this year (SR2014), as we've shipped
    Ruggeduinos instead of the JointIO boards. This commit removes the
    obsolete source code and exports that only related to the JointIO
    board.
    
    Change-Id: I29721c2e8e93853e996e9979d3e01745d47d84d5

--- pyenv/pylib/sr/__init__.py
+++ pyenv/pylib/sr/__init__.py
@@ -1,5 +1,3 @@
 from robot import Robot, NoCameraPresent
-from event import wait_for
-from poll import And, Or, Poll
 from vision import MARKER_ARENA, MARKER_ROBOT, MARKER_SLOT, MARKER_TOKEN_TOP, MARKER_TOKEN_BOTTOM, MARKER_TOKEN_SIDE
 from ruggeduino import INPUT, OUTPUT, INPUT_PULLUP
