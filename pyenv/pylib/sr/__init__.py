1c3a98069cbaea43bf905249af99fa502e4d027c
Rob Spanton <rspanton@zepler.net>
2013-12-29 19:08:26 +0000

    Add brackets to __init__.py imports and wrap
    Long lines of imports can be wrapped by putting the things that are
    being imported in brackets.
    
    Change-Id: If39b4b8a664f3802f9369f696d0da90318990108

--- pyenv/pylib/sr/__init__.py
+++ pyenv/pylib/sr/__init__.py
@@ -1,3 +1,5 @@
-from robot import Robot, NoCameraPresent
-from vision import MARKER_ARENA, MARKER_ROBOT, MARKER_SLOT, MARKER_TOKEN_TOP, MARKER_TOKEN_BOTTOM, MARKER_TOKEN_SIDE
-from ruggeduino import INPUT, OUTPUT, INPUT_PULLUP, Ruggeduino
+from robot import ( Robot, NoCameraPresent )
+from vision import ( MARKER_ARENA, MARKER_ROBOT, MARKER_SLOT,
+                     MARKER_TOKEN_TOP, MARKER_TOKEN_BOTTOM,
+                     MARKER_TOKEN_SIDE )
+from ruggeduino import ( INPUT, OUTPUT, INPUT_PULLUP, Ruggeduino )
